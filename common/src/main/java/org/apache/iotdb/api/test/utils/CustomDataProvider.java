607f089685159a64d320c12f394c761d2fea8397
LinJieXiao-XLJ <1546674204@qq.com>
2025-01-15 10:46:04 +0800

    Fix compilation issues and optimize internal structure

--- common/src/main/java/org/apache/iotdb/api/test/utils/CustomDataProvider.java
+++ common/src/main/java/org/apache/iotdb/api/test/utils/CustomDataProvider.java
@@ -27,15 +27,15 @@ public class CustomDataProvider {
      */
     public Iterable<CSVRecord> readCSV_tree(String filepath, char delimiter) throws IOException {
         // 在Windows中使用
-        String path = Objects.requireNonNull(CustomDataProvider.class.getClassLoader().getResource(filepath)).getPath();
-        if (path.charAt(0) == '/') {
-            path = path.substring(1);
-        }
-        logger.info("read csv:" + path);
-        this.reader = Files.newBufferedReader(Paths.get(path));
+//        String path = Objects.requireNonNull(CustomDataProvider.class.getClassLoader().getResource(filepath)).getPath();
+//        if (path.charAt(0) == '/') {
+//            path = path.substring(1);
+//        }
+//        logger.info("read csv:" + path);
+//        this.reader = Files.newBufferedReader(Paths.get(path));
         // 在Linux中使用
-//        logger.info("read csv:" + CustomDataProvider.class.getClassLoader().getResource(filepath).getPath());
-//        this.reader = Files.newBufferedReader(Paths.get(CustomDataProvider.class.getClassLoader().getResource(filepath).getPath()));
+        logger.info("read csv:" + CustomDataProvider.class.getClassLoader().getResource(filepath).getPath());
+        this.reader = Files.newBufferedReader(Paths.get(CustomDataProvider.class.getClassLoader().getResource(filepath).getPath()));
 
         CSVFormat csvformat = CSVFormat.DEFAULT.withDelimiter(delimiter).withEscape('\\').withQuote('"').withIgnoreEmptyLines(true);
         Iterable<CSVRecord> records = csvformat.parse(reader);
@@ -49,15 +49,15 @@ public class CustomDataProvider {
      */
     public Iterable<CSVRecord> readCSV_table(String filepath) throws IOException {
         // 在Windows中使用
-        String path = Objects.requireNonNull(CustomDataProvider.class.getClassLoader().getResource(filepath)).getPath();
-        if (path.charAt(0) == '/') {
-            path = path.substring(1);
-        }
-        logger.info("read csv:" + path);
-        this.reader = Files.newBufferedReader(Paths.get(path));
+//        String path = Objects.requireNonNull(CustomDataProvider.class.getClassLoader().getResource(filepath)).getPath();
+//        if (path.charAt(0) == '/') {
+//            path = path.substring(1);
+//        }
+//        logger.info("read csv:" + path);
+//        this.reader = Files.newBufferedReader(Paths.get(path));
         // 在Linux中使用
-//        logger.info("read csv:" + CustomDataProvider.class.getClassLoader().getResource(filepath).getPath());
-//        this.reader = Files.newBufferedReader(Paths.get(CustomDataProvider.class.getClassLoader().getResource(filepath).getPath()));
+        logger.info("read csv:" + CustomDataProvider.class.getClassLoader().getResource(filepath).getPath());
+        this.reader = Files.newBufferedReader(Paths.get(CustomDataProvider.class.getClassLoader().getResource(filepath).getPath()));
 
         // 设置CSV文件的格式
         CSVFormat csvformat = CSVFormat.MYSQL;
